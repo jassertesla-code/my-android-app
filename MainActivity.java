5cf4a5a084acf3619bad477d671bad53d93f3908
jassertesla-code <jassertesla@gmail.com>
2025-11-12 17:08:02 +0100

    Add complete test Android project + workflow

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+herepackage com.example.testapk;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello World!");
+        setContentView(tv);
+    }
+}
